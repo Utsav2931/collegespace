b4025fffaa11803b0890e6ee3c13cbb6a0bfc6b8
Utsav2931 <18ce089@charusat.edu.in>
2021-03-17 16:50:40 +0530

    Events and Notes Page added

--- src/containers/ContentBuilder/ContentBuilder.js
+++ src/containers/ContentBuilder/ContentBuilder.js
@@ -3,6 +3,8 @@ import AddPost from "../Add/AddThingsPage";
 import Homescreen from '../HomeScreen/Homescreen';
 import { Route } from 'react-router-dom';
 import Oldpapers from '../Oldpapers/Oldpapers';
+import Events from '../Events/Events'
+import Notes from '../Notes/Notes'
 
 export class ContentBuilder extends Component {
   render() {
@@ -11,7 +13,8 @@ export class ContentBuilder extends Component {
         <Route path="/" exact component={Homescreen}/>
         <Route path="/old-papers" component={Oldpapers}/>
         <Route path="/add-post" component={AddPost}/>
-        
+        <Route path = "/notes" component = {Notes}/>
+        <Route path = "/events" component = {Events}/>
       </div>
     );
   }
