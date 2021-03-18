7e0f4875dd2817cb0a021bc70d05ca7f1493e6c6
smit patel <smitpatel4297@gmail.com>
2021-03-18 13:20:44 +0530

    clubs added

--- src/containers/ContentBuilder/ContentBuilder.js
+++ src/containers/ContentBuilder/ContentBuilder.js
@@ -3,15 +3,20 @@ import AddPost from "../Add/AddThingsPage";
 import Homescreen from '../HomeScreen/Homescreen';
 import { Route } from 'react-router-dom';
 import Oldpapers from '../Oldpapers/Oldpapers';
+import aboutclubs from '../AboutClubs/aboutclubs';
+
 
 export class ContentBuilder extends Component {
   render() {
     return (
       <div>
-        <Route path="/" exact component={Homescreen}/>
-        <Route path="/old-papers" component={Oldpapers}/>
-        <Route path="/add-post" component={AddPost}/>
-        
+        <Route path="/" exact component={Homescreen} />
+        <Route path="/old-papers" component={Oldpapers} />
+        <Route path="/add-post" component={AddPost} />
+        <Route path="/clubs" component={aboutclubs} />
+
+
+
       </div>
     );
   }
